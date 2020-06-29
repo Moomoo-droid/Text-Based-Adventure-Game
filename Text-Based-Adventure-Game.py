a71bf7b36065c75ac3c81b949fdd1496a95d26c6
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-29 07:40:43 +0200

    made question func and started calc func

---
+++ Text-Based-Adventure-Game.py
@@ -0,0 +1,9 @@
+
+
+def question(option1, option2, op1_positive_negative, op2_positive_negative):
+    global option1, option2, op1_positive_negative, op2_positive_negative
+    answer = input(f"what do you want to do ({option1}/{option2}): ")
+    if (op1_positive_negative == "positive") and (answer == option1):
+
+def calc():
+
