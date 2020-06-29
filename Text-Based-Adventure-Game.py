7c1797754474990c3e86db803b2eb20491afd4f2
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-29 10:41:50 +0200

    deleted calc func

--- Text-Based-Adventure-Game.py
+++ Text-Based-Adventure-Game.py
@@ -1,9 +1,6 @@
 
 
-def question(option1, option2, op1_positive_negative, op2_positive_negative):
-    global option1, option2, op1_positive_negative, op2_positive_negative
+def question(option1, option2, op1_positive_negative, op2_positive_negative, win_condition):
+    global answer
     answer = input(f"what do you want to do ({option1}/{option2}): ")
-    if (op1_positive_negative == "positive") and (answer == option1):
-
-def calc():
 
