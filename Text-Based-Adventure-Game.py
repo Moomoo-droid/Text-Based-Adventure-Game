e1c79b735b2ee8cbfe27e039a0e4aada4ab7c6a8
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-29 10:44:48 +0200

    Made some changes

--- Text-Based-Adventure-Game.py
+++ Text-Based-Adventure-Game.py
@@ -1,6 +1,17 @@
 
 
-def question(option1, option2, op1_positive_negative, op2_positive_negative, win_condition):
-    global answer
+def question(option1, option2, op1_positive_negative, op2_positive_negative,
+ win_condition):
     answer = input(f"what do you want to do ({option1}/{option2}): ")
+    if (answer == option1) and (op1_positive_negative == "positive"):
+        return True
+
+    if (answer == option1) and (op1_positive_negative == "negative"):
+        return False
+
+    if (answer == option2) and (op2_positive_negative == "positive"):
+        return True
+
+    if (answer == option2) and (op2_positive_negative == "negative"):
+        return False
 
