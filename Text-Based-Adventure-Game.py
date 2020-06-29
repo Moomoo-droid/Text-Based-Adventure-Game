25c8d8e5432671a2da9dc9e200d79895b3cc04a7
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-29 10:52:20 +0200

    Finished game

--- Text-Based-Adventure-Game.py
+++ Text-Based-Adventure-Game.py
@@ -1,7 +1,6 @@
 
 
-def question(option1, option2, op1_positive_negative, op2_positive_negative,
- win_condition):
+def question(option1, option2, op1_positive_negative, op2_positive_negative):
     answer = input(f"what do you want to do ({option1}/{option2}): ")
     if (answer == option1) and (op1_positive_negative == "positive"):
         return True
@@ -15,3 +14,10 @@ def question(option1, option2, op1_positive_negative, op2_positive_negative,
     if (answer == option2) and (op2_positive_negative == "negative"):
         return False
 
+def game():
+    if question("go_outside", "stay_inside", "positive", "negative"):
+        question("go_outside", "stay_inside", "positive", "negative")
+    
+    else:
+        print("YOU DIED")
+game()
